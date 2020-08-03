a3555aa9674b8f9b903f370232784d7abf9f624c
code-JOA <joshbillion1@gmail.com>
2020-08-03 17:21:51 +0100

    sorted functions for 2nd preprocessing , cleaning movie data

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+
 # fucntion to replace Missing values ie all Nan in the df with Unknown
 def fill_na(data):
     """Iterate through columns , if any nulls replace with "Unkonwn" """
@@ -10,7 +11,12 @@ def fill_na(data):
 # data = fill_na(data)
 
 
-def make_genrelist(x):
+import numpy as np
+import pandas as pd
+
+
+def Make_genrelist(x):
+
     gen = []
     st = " "
     for i in x:
@@ -23,3 +29,59 @@ def make_genrelist(x):
         return np.NaN
     else:
         return (st.join(gen))
+    
+    
+
+def get_1st_actor(x):
+    """get first actor from list of casts """  
+
+    casts=[]
+    for i in x:
+        casts.append(i.get("name"))
+    if casts == []:
+        return np.NaN
+    else:
+        return (casts[0])
+    
+    
+def get_2nd_actor(x):
+    """get second actor from list of casts """  
+
+    casts=[]
+    for i in x:
+        casts.append(i.get("name"))
+    if casts == [] or len(casts)<=1:
+        return np.NaN
+    else:
+        return (casts[1])
+    
+    
+def get_3rdactor(x):
+    """get third actor from list of casts """  
+    casts=[]
+    for i in x:
+        casts.append(i.get("name"))
+    if casts == [] or len(casts)<=2:
+        return np.NaN
+    else:
+        return (casts[2])
+
+    
+
+def get_directors(x):
+    directors = []
+    st = ""
+    for i in x:
+        if i.get("job") == "Director":
+            directors.append(i.get("name"))
+    if directors == []:
+        return np.NaN
+    else:
+        return (st.join(directors))
+    
+    
+    
+    
+    
+    
+    
